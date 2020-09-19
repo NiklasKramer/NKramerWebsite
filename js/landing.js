4dc4d98f08edaac46bb48919a4451ef8e9a40eb4
Niklas Kramer <43847968+NiklasKramer@users.noreply.github.com>
2020-09-19 15:15:23 +0200

    basic implementation of landingarea

---
+++ js/landing.js
@@ -0,0 +1,14 @@
+const enterPage = () => {
+    const enterWebiste = document.querySelector('#LANDING h1')
+
+    enterWebiste.addEventListener('click', () => {
+        console.log()
+        document.querySelector('#LANDING').style.display = "none"
+        document.querySelector('#CONTENT').style.display = "block"
+        document.querySelector('body').style.background= "rgb(243, 243, 243)"
+
+    })
+
+}
+
+enterPage()
