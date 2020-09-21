a5304604523a2243627e78770d285625243f487a
Niklas Kramer <43847968+NiklasKramer@users.noreply.github.com>
2020-09-21 19:18:12 +0200

    added scrolling animation

--- js/landing.js
+++ js/landing.js
@@ -1,16 +1,31 @@
 const enterPage = () => {
-    const enterWebiste = document.querySelector('#LANDING')
 
+
+    // Enter through click
+    const enterWebiste = document.querySelector('#LANDING')
     enterWebiste.addEventListener('click', () => {
         //document.querySelector('#LANDING').style.display = "none"
         document.querySelector('#CONTENT').style.display = "block"
         document.querySelector('body').style.background= "rgb(243, 243, 243)"
         document.querySelector('#LANDING').style.opacity =  '0'
         document.querySelector('#LANDING').style.pointerEvents =  'none'
-
-
-
-
     })
+
+
+    // Enter through scroll
+    let counter = 0;
+    window.addEventListener('scroll', ()=>{
+        if (counter<=100) {
+            document.querySelector('#LANDING').style.opacity = (100 - counter).toString()
+            counter= counter + 25;
+        }
+        else{
+            document.querySelector('#CONTENT').style.display = "block"
+            document.querySelector('body').style.background= "rgb(243, 243, 243)"
+            document.querySelector('#LANDING').style.opacity =  '0'
+            document.querySelector('#LANDING').style.pointerEvents =  'none'
+        }
+    })
+
 }
 enterPage()
