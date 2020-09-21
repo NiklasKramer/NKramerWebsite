b8c9a066ff0fc594d26f2c2f45aaddbed4c6c0d9
Niklas Kramer <43847968+NiklasKramer@users.noreply.github.com>
2020-09-21 10:55:23 +0200

    mobile menu fixed

--- js/navigation.js
+++ js/navigation.js
@@ -3,6 +3,21 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links')
     const navLinks = document.querySelectorAll('.nav-links li')
 
+
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            nav.classList.toggle('nav-active')
+            burger.classList.toggle('toggle');
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+                }
+            })
+        });
+    });
+
     burger.addEventListener('click', () => {
         //Toggle Nav
         nav.classList.toggle('nav-active')
